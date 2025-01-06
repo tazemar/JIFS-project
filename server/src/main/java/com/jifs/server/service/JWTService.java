21f6d9f24120f93da71655758d3776ec70ecc782
Thibault Azemar <thibault.azemar@thalesgroup.com>
2025-01-06 16:29:44 +0100

    tech: generate token

--- server/src/main/java/com/jifs/server/service/JWTService.java
+++ server/src/main/java/com/jifs/server/service/JWTService.java
@@ -2,5 +2,5 @@ package com.jifs.server.service;
 
 public interface JWTService {
 
-    String generateToken();
+    String generateToken(String username);
 }
--- server/src/main/java/com/jifs/server/service/impl/JWTServiceImpl.java
+++ server/src/main/java/com/jifs/server/service/impl/JWTServiceImpl.java
@@ -1,12 +1,52 @@
 package com.jifs.server.service.impl;
 
 import com.jifs.server.service.JWTService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTServiceImpl implements JWTService {
+
+    private String secretKey = "";
+
+    public JWTServiceImpl() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGenerator.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
-    public String generateToken() {
-        return "faketokentemp";
+    public String generateToken(String username) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .and()
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    private Key getSecretKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 }
