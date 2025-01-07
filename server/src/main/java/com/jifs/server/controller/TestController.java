43cfefbabac572058d2b548f0bc9ffe9e039c6ab
Thibault Azemar <thibault.azemar@thalesgroup.com>
2025-01-07 13:56:36 +0100

    tech: refacto and change path to url/api

--- server/src/main/java/com/jifs/server/controller/TestController.java
+++ server/src/main/java/com/jifs/server/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api/test")
 @AllArgsConstructor
 @PreAuthorize("hasRole('USER')")
 public class TestController {
