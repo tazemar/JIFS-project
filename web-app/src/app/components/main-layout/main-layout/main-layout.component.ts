3ddde80780a846a9550b46471b40a55f30e86cbb
Thibault Azemar <thibault.azemar@thalesgroup.com>
2025-01-09 16:04:09 +0100

    tech : remove all modules

--- web-app/src/app/components/main-layout/main-layout/main-layout.component.ts
+++ web-app/src/app/components/main-layout/main-layout/main-layout.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  standalone: false,
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
