f2499d24bac32edeccaab01f61db09b973b3c459
Thibault Azemar <thibault.azemar@thalesgroup.com>
2025-01-14 11:52:04 +0100

    feat: adding checking

--- web-app/src/app/components/auth/create-account/create-account.component.ts
+++ web-app/src/app/components/auth/create-account/create-account.component.ts
@@ -1,38 +1,56 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {SvgIconComponent} from 'angular-svg-icon';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../../core/services/auth.service';
-import {Router} from '@angular/router';
-import {ErrorPopupComponent} from '../../../shared/components/error-popup/error-popup.component';
+import {Router, RouterLink} from '@angular/router';
 import {ErrorService} from '../../../shared/components/error-popup/error.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss',
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    RouterLink
   ]
 })
-export class CreateAccountComponent {
+export class CreateAccountComponent implements OnInit {
 
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  createAccountForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router, private errorService: ErrorService) {}
+  constructor(private authService: AuthService,
+              private router: Router,
+              private errorService: ErrorService,
+              private fb: FormBuilder) {
+    this.createAccountForm = new FormGroup({});
+  }
+
+  ngOnInit(): void {
+    this.createAccountForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  isFormValid(): boolean {
+    return this.createAccountForm.valid;
+  }
 
   async onSubmit(): Promise<void> {
-    if (this.username && this.email && this.password) {
+    if (this.isFormValid()) {
       try {
-        this.authService.createAccount(this.username, this.email, this.password).subscribe({
+        const formValues = this.createAccountForm.value;
+        this.authService.createAccount(formValues.username, formValues.email, formValues.password).subscribe({
           next: (response) => {
             console.log(response);
           },
           error: (e) => {
-            console.error(e);
-            this.errorService.showError(e);
+            const errorObj = JSON.parse(e.error);
+            console.error(errorObj);
+            this.errorService.showError(errorObj.description);
           },
           complete: () => this.router.navigate([''])
         })
--- web-app/src/app/components/auth/login/login.component.ts
+++ web-app/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../../core/services/auth.service';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {NgIf} from '@angular/common';
 import {SvgIconComponent} from "angular-svg-icon";
+import e, {response} from 'express';
+import {ErrorService} from '../../../shared/components/error-popup/error.service';
 
 @Component({
   selector: 'app-login',
@@ -12,35 +14,56 @@ import {SvgIconComponent} from "angular-svg-icon";
   imports: [
     RouterLink,
     FormsModule,
-    SvgIconComponent
+    SvgIconComponent,
+    ReactiveFormsModule,
+    NgIf
   ],
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  loginAccountForm: FormGroup;
 
-  token$!: Observable<string>;
+  constructor(private authService: AuthService,
+              private router: Router,
+              private errorService: ErrorService,
+              private fb: FormBuilder) {
+    this.loginAccountForm = new FormGroup({});
+  }
 
-  constructor(private authService: AuthService, private router: Router) { }
+  ngOnInit(): void {
+    this.loginAccountForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
   async onSubmit(): Promise<void> {
-    if (this.email && this.password) {
-      try {
-        this.authService.login(this.email, this.password).subscribe({
-          next: (response) => {
-            this.authService.storeToken(response);
-          },
-          error: (e) => console.error(e),
-          complete: () => this.router.navigate([''])
-        })
-      } catch (error) {
-        this.errorMessage = 'Erreur lors de la connexion';
+    if (this.isFormValid()) {
+      const formValues = this.loginAccountForm.value;
+      if (formValues.email && formValues.password) {
+        try {
+          this.authService.login(formValues.email, formValues.password).subscribe({
+            next: (response) => {
+              this.authService.storeToken(response);
+            },
+            error: (e) => {
+              const errorObj = JSON.parse(e.error);
+              console.error(errorObj);
+              this.errorService.showError(errorObj.description);
+            },
+            complete: () => this.router.navigate([''])
+          })
+        } catch (error) {
+          this.errorService.showError('Erreur lors de la connexion');
+        }
+      } else {
+        this.errorService.showError('Email et mot de passe requis!');
       }
-    } else {
-      this.errorMessage = 'Email et mot de passe requis!';
     }
+  }
+
+  isFormValid(): boolean {
+    return this.loginAccountForm.valid;
   }
 }
